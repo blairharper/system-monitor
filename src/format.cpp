3c38d44babbe72a7aacaacdc2d4b664f678e14db
Blair Harper <blair@passio.co.uk>
2021-08-10 13:27:29 +0100

    implement elapsed time formatter

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,22 @@
 #include <string>
 
 #include "format.h"
+#include "math.h"
 
 using std::string;
+using std::to_string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  int daySeconds = 86400;
+  int hourSeconds = 3600;
+  int minuteSeconds = 60;
+
+  int hours = floor(seconds / hourSeconds);
+  int minutes = floor((seconds - (hours * hourSeconds)) / minuteSeconds);
+  seconds =
+      floor((seconds - (hours * hourSeconds) - (minutes * minuteSeconds)));
+
+  return to_string(hours) + ":" + to_string(minutes) + ":" + to_string(seconds);
+}
